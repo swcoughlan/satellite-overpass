900fa598df2dd6e885f52724d1a2df9bb50bce09
Steve <s.w.coughlan@gmail.com>
2023-10-19 11:50:14 +0100

    Updated satellite-overpass.ipynb and satellite-overpass.py

--- satellite-overpass.py
+++ satellite-overpass.py
@@ -1,33 +1,33 @@
-# import required packages
+# Import required packages
 import requests
 import pandas as pd
+from datetime import datetime
+from pytz import timezone
 import config
 
-# pull overpass data from Spectator.Earth API
-api_key = config.api_key 
-bbox = '-6.15450317,53.30408682,-6.07896861,53.34080955' # Dublin Bay
-satellites = 'Sentinel-1A, Sentinel-1B, Sentinel-2A, Sentinel-2B, Sentinel-3A, Sentinel-3B,Landsat-8'
-url = 'https://api.spectator.earth/overpass/?api_key={api_key}&bbox={bbox}&satellites={satellites}'.format(api_key=api_key, bbox=bbox, satellites=satellites)
+# Pull overpass data from Spectator.Earth API
+api_key = config.api_key # Insert your api code here
+bbox = '-6.15450317,53.30408682,-6.07896861,53.34080955'  # Dublin Bay
+satellites = 'Sentinel-1A, Sentinel-1B, Sentinel-2A, Sentinel-2B, Sentinel-3A, Sentinel-3B, Landsat-8'
+url = f'https://api.spectator.earth/overpass/?api_key={api_key}&bbox={bbox}&satellites={satellites}'
 response = requests.get(url)
 data = response.json()
 
-# loop through results to extract date and satellite info
-result = []
-for i in range(len(data['overpasses'])):
-  overpass = {}
-  overpass['date'] = data['overpasses'][i]['date']
-  overpass['satellite'] = data['overpasses'][i]['satellite']
-  result.append(overpass)
+# Extract date and satellite info
+overpasses = [{'date': overpass['date'], 'satellite': overpass['satellite']} for overpass in data['overpasses']]
+print(overpasses)
 
-# create dataframe of results & convert datetime to day, date, Irish Standard Time, Coordinated Universal Time
-data = pd.DataFrame(result, columns=['satellite', 'date'])
-data['Day'] = pd.to_datetime(data['date']).dt.day_name()
-data['Date'] = pd.to_datetime(data['date']).dt.date
-data['IST'] = pd.DatetimeIndex(pd.to_datetime(data['date'])).tz_convert('Europe/Dublin')
-data['IST'] = pd.to_datetime(data['IST']).dt.strftime('%H:%M:%S')
-data['UTC'] = pd.to_datetime(data['date']).dt.strftime('%H:%M:%S')
-data.pop('date') 
-data = data.rename(columns={'satellite': 'Satellite'})
+# Create a dataframe of results and convert dates
+data = pd.DataFrame(overpasses)
+data['datetime'] = pd.to_datetime(data['date'])
+data['day'] = data['datetime'].dt.day_name()
+data['date'] = data['datetime'].dt.date
 
-# export to csv
+# Convert datetime to Irish Standard Time (IST) and Coordinated Universal Time (UTC) and add to dataframe
+ist_timezone = timezone('Europe/Dublin')
+data['ist'] = data['datetime'].apply(lambda x: x.astimezone(ist_timezone).strftime('%H:%M:%S'))
+data['utc'] = data['datetime'].dt.strftime('%H:%M:%S')
+data.drop(columns=['datetime'], axis=1, inplace=True) # datetime column no longer needed
+
+# Export to CSV
 data.to_csv('output/sample.csv', index=False)
